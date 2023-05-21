e6b1c94f0ec807e983fb2bc77c1f6c22d4a5e6fe
Coope <Coope@100.81.32.102>
2023-05-20 18:23:02 -0700

    worked on a bunch of shit

--- WTHU/src/WTHU.java
+++ WTHU/src/WTHU.java
@@ -17,6 +17,8 @@ public class WTHU {
     private static TimerTask task;
     private static JFrame frame;
     
+    static boolean isAlarmAM;
+    
     
 
     public static void main(String[] args) {
@@ -28,29 +30,37 @@ public class WTHU {
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         JPanel panel = new JPanel();
-        JButton button = new JButton("Set Timer");
+        JButton button = new JButton("Set Alarm");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 setTimerDuration();
             }
         });
+        
+
+    	
 
         panel.add(button);
         frame.add(panel);
 
         frame.setVisible(true); // Make the frame visible
     }
-    
 
     private static void setTimerDuration() {
+
     	
     	//user input alarm time
-        String input = JOptionPane.showInputDialog(frame, "Enter Hour and Minute (HH:MM)");
+        String input = JOptionPane.showInputDialog(frame, "Set Alarm (HH:MM:AM/PM)");
         String[] time = input.split(":");
         int alarmHour = Integer.parseInt(time[0]); // 6:30 7:28
         int alarmMinute = Integer.parseInt(time[1]);
-        boolean isAlarmAM;
+        String amPm = time[2];
+        
+        if(amPm.equals("PM")) {
+        	alarmHour +=12; // convert back to military
+        	
+        }
         
         if(alarmHour >= 12) { // if noon or past 
         	isAlarmAM = false; // then it's PM
@@ -59,11 +69,14 @@ public class WTHU {
         }else { // is hour is < 12 
         	isAlarmAM = true; // then it's AM
         }
-                      
+                
+        
+        System.out.println("Alarm set for: " + alarmHour + ":" + alarmMinute + " " + amPm);
+
         // localTime will be startTime of timer
         
         LocalTime currentTime = LocalTime.now(); // grab local time in military time
-        String localTime = currentTime.getHour() + ":" + currentTime.getMinute(); // should return ex: 8:30
+       // String localTime = currentTime.getHour() + ":" + currentTime.getMinute(); // should return ex: 8:30
         int currHour = currentTime.getHour();
         int currMin = currentTime.getMinute();
         
@@ -92,14 +105,16 @@ public class WTHU {
         		durationH = Math.abs(currHour - alarmHour);
         	}
         	
-        	durationH *= 1000*60*60*60; // int 8 >  8000 ms > 8sec > 8*60 min > 8*60*60 hr in ms
-        	durationM *= 1000*60*60;
+        	durationH *= 1000*60*60; //in *1000 = secs * 60 = min *60 = hr
+        	durationM *= 1000*60;
         	
         	int duration = durationH + durationM; // duration in MS
+        	System.out.println(currHour + ":" + currMin);
+        	System.out.println(duration + " in MS");
             
             
             
-            System.out.println("Timer set for: " + duration);
+            System.out.println("Timer set for: " + duration/1000 + " seconds");
             if (duration > 0) {
                 startTimer(duration);
             } else {
